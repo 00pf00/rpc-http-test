080ff74beedd7d3645890338adda5158b9e21a5b
feeli <feeli@tencent.com>
2020-03-31 13:28:19 +0800

    debug JsonArray

--- http-client/src/main/java/com/bupt/HttpClient.java
+++ http-client/src/main/java/com/bupt/HttpClient.java
@@ -12,39 +12,39 @@ public class HttpClient {
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public static void main(final String[] args) {
-        final CountDownLatch countDownLatch = new CountDownLatch(2);
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
         Integer tenantId = 60;
         OkHttpClient ht = new OkHttpClient();
-//        Request req = new Request.Builder().url("http://127.0.0.1:8080/device").method("GET", null).build();
-        JSONObject jobj = new JSONObject();
-
-//      jobj.put("id", UUID.randomUUID().toString());
-        jobj.put("name", UUID.randomUUID().toString());
-        jobj.put("tenantId",tenantId);
-//        DeviceInfoProto.DeviceInfo.Builder builder = DeviceInfoProto.DeviceInfo.newBuilder();
-//        builder.setId("b");
-//        builder.setName("B");
-//        DeviceInfoProto.DeviceInfo reqs = builder.build();
-//        JSONObject jobj = JSONObject.;
-//        System.out.println(reqs);
-        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jobj.toJSONString());
-        Request req = new Request.Builder().url("http://127.0.0.1:8100/api/v1/info/saveDevice").method("POST", body).build();
-        Call call = ht.newCall(req);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    System.out.println(response.body().string());
-                    System.out.println("step 1 : 请求返回!");
-                }
-                countDownLatch.countDown();
-            }
-        });
+////        Request req = new Request.Builder().url("http://127.0.0.1:8080/device").method("GET", null).build();
+//        JSONObject jobj = new JSONObject();
+//
+////      jobj.put("id", UUID.randomUUID().toString());
+//        jobj.put("name", UUID.randomUUID().toString());
+//        jobj.put("tenantId",tenantId);
+////        DeviceInfoProto.DeviceInfo.Builder builder = DeviceInfoProto.DeviceInfo.newBuilder();
+////        builder.setId("b");
+////        builder.setName("B");
+////        DeviceInfoProto.DeviceInfo reqs = builder.build();
+////        JSONObject jobj = JSONObject.;
+////        System.out.println(reqs);
+//        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jobj.toJSONString());
+//        Request req = new Request.Builder().url("http://127.0.0.1:8100/api/v1/info/saveDevice").method("POST", body).build();
+//        Call call = ht.newCall(req);
+//        call.enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                if (response.isSuccessful()) {
+//                    System.out.println(response.body().string());
+//                    System.out.println("step 1 : 请求返回!");
+//                }
+//                countDownLatch.countDown();
+//            }
+//        });
         OkHttpClient rht = new OkHttpClient();
         Request rreq = new Request.Builder().url("http://127.0.0.1:8100/api/v1/info/allDevice/"+tenantId).method("POST", body).build();
         Call rcall = rht.newCall(rreq);
